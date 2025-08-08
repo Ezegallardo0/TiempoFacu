2a185184b9cd7be186911883f13447f5f0cf5b97
Ezegallardo0 <plumiferogaming@gmail.com>
2025-08-08 17:39:17 -0300

    "error 404"

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 import Login from './components/login';
 import Register from './components/Register';
 
+
 function App() {
 
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/calendario" element={<Calendario />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </main>
       <Footer />
